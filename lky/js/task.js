2fe6d8b43fae4fe3d2681095c2c26ba892209072
qnsekddl1 <qnsekddl1@naver.com>
2024-01-28 19:07:31 +0900

    이기영 01281907

---
+++ lky/js/task.js
@@ -0,0 +1,59 @@
+// comments에서 name 추출하기
+
+// fetch("https://jsonplaceholder.typicode.com/comments")
+//     .then((response) => response.json())
+//     .then((comments) => {
+//         comments.forEach((comments) => {
+//             console.log(comments.name);
+//         });
+//     });
+
+// comments에서 email 추출하기
+
+// fetch("https://jsonplaceholder.typicode.com/comments")
+//     .then((response) => response.json())
+//     .then((comments) => {
+//         comments.forEach((comments) => {
+//             console.log(comments.email);
+//         });
+//     });
+
+// user에서 compant name 추출하기
+
+// fetch("https://jsonplaceholder.typicode.com/users")
+//     .then((response) => response.json())
+//     .then((users) => {
+//         users.forEach((user) => {
+//             console.log(user.company.name);
+//         });
+//     });
+
+// posts에서 title 추출하기
+
+// fetch("https://jsonplaceholder.typicode.com/posts")
+//     .then((response) => response.json())
+//     .then((posts) => {
+//         posts.forEach((posts) => {
+//             console.log(posts.title);
+//         });
+//     });
+
+// albums에서 title 추출하기
+
+// fetch("https://jsonplaceholder.typicode.com/albums")
+//     .then((response) => response.json())
+//     .then((albums) => {
+//         albums.forEach((albums) => {
+//             console.log(albums.title);
+//         });
+//     });
+
+// photos에서 url 추출하기
+
+// fetch("https://jsonplaceholder.typicode.com/photos")
+//     .then((response) => response.json())
+//     .then((photos) => {
+//         photos.forEach((photos) => {
+//             console.log(photos.url);
+//         });
+//     });
